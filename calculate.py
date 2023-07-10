f8dbb4055b1dc5c7165b93ec7ccbf0378ccb7219
WitekHun <124146614+WitekHun@users.noreply.github.com>
2023-07-10 12:51:47 +0200

    Chnge order and fail again

--- calculate.py
+++ calculate.py
@@ -4,20 +4,35 @@ operator = None
 wait_for_number = True
 
 while True:
-    if wait_for_number == True:
+    if wait_for_number:
         try:
-            operand = float(input("Wprowadź liczbę "))
-            wait_for_number = False
+            operand = float(input("Wprowadź liczbę:  "))
             if result == None:
                 result = operand
-                print(f"RESULT {result}")
-            continue
+                print(
+                    f"Wartość RESULT przy wprowadzaniu liczby: {result}, wartość operandu {operand}"
+                )  # only for debuging
+                wait_for_number = False
+                continue
+            else:
+                print(
+                    f"Wartość2 RESULT przy wprowadzaniu liczby: {result}, wartość2 operandu {operand}"
+                )  # only for debugin
+                wait_for_number = False
+                continue
         except ValueError:
             print("Błędna wartość! ")
             continue
     else:
         operator = input("'+', '-', '*', '/' lub '=' to finish ")
-
+        print(
+            f"Wartość result po wprowadzeniu operatora {result}, wartość operandu {operand}"
+        )  # only for debuging
+        """if result == None:
+            result = operand
+            wait_for_number = True
+            continue
+        else:"""
         if operator == "+":
             result += operand
             wait_for_number = True
